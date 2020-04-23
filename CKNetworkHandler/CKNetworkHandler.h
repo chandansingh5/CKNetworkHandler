66301c59b1990ffd2452fd514f0990c967777713
chandan <chandansingh.it13@gmail.com>
2020-04-23 15:35:39 +0530

    Initial Commit

---
+++ CKNetworkHandler/CKNetworkHandler.h
@@ -0,0 +1,19 @@
+//
+//  CKNetworkHandler.h
+//  CKNetworkHandler
+//
+//  Created by Chandan Kumar on 23/04/20.
+//  Copyright © 2020 Chandan Kumar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CKNetworkHandler.
+FOUNDATION_EXPORT double CKNetworkHandlerVersionNumber;
+
+//! Project version string for CKNetworkHandler.
+FOUNDATION_EXPORT const unsigned char CKNetworkHandlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CKNetworkHandler/PublicHeader.h>
+
+
